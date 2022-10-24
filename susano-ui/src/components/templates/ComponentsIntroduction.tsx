6f07fa8f97b933223ab84e9e2d0af05f03345391
渡邉将太 <shotawebcoding@gmail.com>
2022-10-25 00:00:40 +0900

    Update

--- susano-ui/src/components/templates/ComponentsIntroduction.tsx
+++ susano-ui/src/components/templates/ComponentsIntroduction.tsx
@@ -8,6 +8,16 @@ import * as Css from './componentsIntroductionStyle';
 const ComponentsIntroduction = () => {
   return (
       <div css={Css.PageContainer}>
+          <div css={Css.DocumentContainer}>
+              <h3>Susanoo's Document</h3>
+              <p>
+                  Susanoo UI is a modern Japanese style component composed of React/typescript.
+                  <br />
+                  Developed from scratch by a young Japanese designer.
+              </p>
+          </div>
+
+
           <div css={Css.PageTitleContainer}>
               <h3>SUSANOO Components</h3>
               <p>Susanoo UI provides various template components.</p>
--- susano-ui/src/components/templates/componentsIntroductionStyle.tsx
+++ susano-ui/src/components/templates/componentsIntroductionStyle.tsx
@@ -3,10 +3,33 @@ import { css } from '@emotion/react';
 export const PageContainer = css`
     padding: 20px;
 `;
+export const DocumentContainer = css`
+    h3{
+        margin: 20px 0 0 0;
+        padding: 0;
+        font-weight: 700;
+        font-size: 1.5rem;
+        color: #fff;
+    }
+
+     p{
+        padding: 24px;
+        background-color: rgba(145, 158, 171, 0.12);
+        margin-top: 20px;
+        font-size: 1.0rem;
+        font-weight: 500;
+        color: #f8f8f8;
+        border: 1px dashed rgba(145, 158, 171, 0.24);
+        border-radius: 12px;
+        line-height: 45px;
+    }
+`;
+
+
 
 export const PageTitleContainer = css`
     h3{
-        margin: 20px 0 0 0;
+        margin: 50px 0 0 0;
         padding: 0;
         font-weight: 700;
         font-size: 1.5rem;
@@ -48,7 +71,7 @@ export const ComponentsParts = css`
 `;
 
 export const ConponentsContaoner = css`
-    margin-top: 20px;
+    margin-top: 50px;
     width: 100%;
     display: flex;
     flex-wrap: wrap;
