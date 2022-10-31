86921f01902a33425bc68f16d682afafdd394d3e
渡邉将太 <shotawebcoding@gmail.com>
2022-10-31 23:52:41 +0900

    update

--- susano-ui/src/components/pages/home/Home.tsx
+++ susano-ui/src/components/pages/home/Home.tsx
@@ -1,14 +1,28 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import {jsx} from "@emotion/react";
 import React from 'react'
 import Header from '../../organisums/header/Header'
 import FooterTemplate from '../../templates/footer/FooterTemplate'
+
+import * as Css from './homeStyle';
 
 const Home = () => {
   return (
       <div>
           <Header />
           <div>
-              {/* Homeのコンポーネント */}
-          </div>
+          <div css={Css.mainTitleComtainer}>
+              <h1>
+                  Even if it's a <span>big problem</span>, <br />
+                  there's nothing you can't solve.
+              </h1>
+              <p>
+                  SUSANOO UI is a component library application <br />
+                  using modern front-end technology.
+              </p>
+              </div>
+              </div>
           <FooterTemplate />
     </div>
   )
---
+++ susano-ui/src/components/pages/home/homeStyle.tsx
@@ -0,0 +1,21 @@
+import { css } from "@emotion/react";
+
+export const mainTitleComtainer = css`
+padding: 200px 80px;
+h1{
+    color: #fff;
+    font-size: 2.5rem;
+    font-weight: bold;
+
+    span{
+        color: rgb(250, 84, 28);
+    }
+}
+
+p{
+    margin-top: 10px;
+    font-size: 1.125rem;
+    font-weight: bold;
+    color: rgb(145, 158, 171);
+}
+`;
