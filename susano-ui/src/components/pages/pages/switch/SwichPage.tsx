091a52710b03a953b1020cb98227756cbe01f074
渡邉将太 <shotawebcoding@gmail.com>
2022-11-17 22:15:05 +0900

    updated

--- susano-ui/src/components/pages/pages/switch/SwichPage.tsx
+++ susano-ui/src/components/pages/pages/switch/SwichPage.tsx
@@ -1,12 +1,21 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import {jsx} from "@emotion/react";
 import React from 'react'
 import Header from '../../../organisums/header/Header'
 import FooterTemplate from '../../../templates/footer/FooterTemplate'
+import * as Css from './swichPageStyle';
 
 const SwichPage = () => {
     return (
         <div>
             <Header />
-            <div>
+            <div css={Css.PageSectionContainer}>
+                <div css={Css.titleAreaContainer}>
+                    <h2>Switch</h2>
+                    <p>Switches toggle the state of a single setting on or off.</p>
+                </div>
+                
                 
             </div>
             <FooterTemplate />
---
+++ susano-ui/src/components/pages/pages/switch/swichPageStyle.tsx
@@ -0,0 +1,20 @@
+import { css } from '@emotion/react'
+
+export const PageSectionContainer = css`
+padding:  50px 100px;
+`;
+
+
+export const titleAreaContainer = css`
+    h2{
+        color: #fff;
+        font-size: 2.5rem;
+        font-weight: bold;
+    }
+    p{
+        margin-top: 10px;
+        font-size: 1rem;
+        color: rgb(145, 158, 171);
+}
+    }
+`;
